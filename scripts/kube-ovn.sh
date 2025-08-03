a2bc8bb04dccaa539688f027a074d35b34fba3bb
Kevin Carter <kevin.carter@rackspace.com>
2025-08-03 01:10:38 -0500

    fix: add cache busting where missing
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

--- scripts/kube-ovn.sh
+++ scripts/kube-ovn.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-if [ ${KUBE_OVN_VERSION:-master} = "v1.14.4" ]; then
+if [ ${KUBE_OVN_VERSION_ENV:-master} = "v1.14.4" ]; then
     # CVE fixes CVE-2025-54388,CVE-2025-22870,CVE-2025-22872,CVE-2025-22868
     go get -u github.com/docker/docker
     go get -u golang.org/x/net
     go get -u golang.org/x/oauth2
-elif [ ${KUBE_OVN_VERSION:-master} = "v1.13.14" ]; then
-    # CVE fixes CVE-2025-22870,CVE-2025-22872,CVE-2025-22868
+elif [ ${KUBE_OVN_VERSION_ENV:-master} = "v1.13.14" ]; then
+    # CVE fixes CVE-2025-22870,CVE-2025-22872,CVE-2025-22868,GHSA-fv92-fjc5-jj9h
+    go get -u github.com/go-viper/mapstructure/v2
     go get -u golang.org/x/net
     go get -u golang.org/x/oauth2
 fi
