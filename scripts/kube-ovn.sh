2689c7e950aadb87eeaef94f2f7ef4764c376101
Chris Breu <chris.breu@rackspace.com>
2025-11-13 14:49:16 -0600

    fix: bump kube-ovn to 1.14.10

--- scripts/kube-ovn.sh
+++ scripts/kube-ovn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ${KUBE_OVN_VERSION_ENV:-master} = "v1.14.4" ]; then
+if [ ${KUBE_OVN_VERSION_ENV:-master} = "v1.14.10" ]; then
     # CVE fixes CVE-2025-54388,CVE-2025-22870,CVE-2025-22872,CVE-2025-22868
     go get -u github.com/docker/docker
     go get -u golang.org/x/net
