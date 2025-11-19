65fe05909a43f9997e29f01b99b7183ec89bb2b3
Chris Breu <chris.breu@rackspace.com>
2025-11-19 10:39:08 -0600

    fix: add supoort for v1.14.15 and drop support for v1.14.10

--- scripts/kube-ovn.sh
+++ scripts/kube-ovn.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-if [ ${KUBE_OVN_VERSION_ENV:-master} = "v1.14.10" ]; then
+if [ ${KUBE_OVN_VERSION_ENV:-master} = "v1.14.15" ]; then
+    # CVE fixes CVE-2025-54388,CVE-2025-22870,CVE-2025-22872,CVE-2025-22868
+    #           CVE-2024-25621
+    go get -u github.com/docker/docker
+    go get -u golang.org/x/net
+    go get -u golang.org/x/oauth2
+elif [ ${KUBE_OVN_VERSION_ENV:-master} = "v1.14.10" ]; then
     # CVE fixes CVE-2025-54388,CVE-2025-22870,CVE-2025-22872,CVE-2025-22868
     go get -u github.com/docker/docker
     go get -u golang.org/x/net
