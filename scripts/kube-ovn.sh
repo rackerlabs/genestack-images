69df9dabca492826bf6b473cd443e3719e6283a3
Adam Fabian <adam.fabian@rackspace.com>
2026-03-09 14:14:51 -0500

    fix(kube-ovn): remediate CVE-2026-24051 in go.opentelemetry.io/otel/sdk for Kube-OVN v1.15.4 build
    otherwise, trivy scan shows only Ubuntu base CVEs without currently
    available package fixes of MEDIUM severity and Go stdlib UNKNOWN
    severity stuff
    
    trivy image --input kube-ovn-v1.15.4-local.tar --scanners vuln \
    --severity HIGH,CRITICAL shows:
    
    0 high, 0 critical

--- scripts/kube-ovn.sh
+++ scripts/kube-ovn.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-if [ ${KUBE_OVN_VERSION_ENV:-master} = "v1.14.15" ]; then
+if [ ${KUBE_OVN_VERSION_ENV:-master} = "v1.15.4" ]; then
+    # CVE fixes CVE-2026-24051
+    go get -u go.opentelemetry.io/otel/sdk@v1.40.0
+elif [ ${KUBE_OVN_VERSION_ENV:-master} = "v1.14.15" ]; then
     # CVE fixes CVE-2025-54388,CVE-2025-22870,CVE-2025-22872,CVE-2025-22868
     #           CVE-2024-25621
     go get -u github.com/docker/docker
